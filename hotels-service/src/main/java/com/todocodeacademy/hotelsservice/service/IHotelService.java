11da2103de2dd8a92a4d7efe2aa60fe8bade49a6
Eugenio Morales <eugeniomorales@MacBook-Air-de-Eugenio.local>
2023-11-24 15:02:48 -0300

    first commit of config server

---
+++ hotels-service/src/main/java/com/todocodeacademy/hotelsservice/service/IHotelService.java
@@ -0,0 +1,7 @@
+package com.todocodeacademy.hotelsservice.service;
+import com.todocodeacademy.hotelsservice.model.Hotel;
+import java.util.List;
+
+public interface IHotelService {
+    public List<Hotel> getHotelsByCityId(Long city_id);
+}
